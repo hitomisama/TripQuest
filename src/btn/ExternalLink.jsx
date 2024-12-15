d070832b9d1138a132786bd82e77d6e95e1abce8
Yisongjing <24aw0105@jec.ac.jp>
2024-12-15 19:02:29 +0900

    测试

--- src/btn/ExternalLink.jsx
+++ src/btn/ExternalLink.jsx
@@ -1,20 +1,19 @@
 import React from "react";
+import "../css/ExternalLink.css";
 
 function ExternalLink() {
+  const handleClick = () => {
+    // 使用 window.open 打开新页面
+    // window.open("https://www.baidu.com", "_blank", "noopener,noreferrer");
+  };
+
   return (
-    <>
-      <div className="btn">
-        <div
-          className="more"
-          href="https://example.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h4> 他のクエストは公式サイトでチェック</h4>
-          <div className="arrow"></div>
-        </div>
+    <div className="btn_more" onClick={handleClick}>
+      <div className="more">
+        <h4>他のクエストは公式サイトでチェック</h4>
+        <div className="arrow"></div>
       </div>
-    </>
+    </div>
   );
 }
 
