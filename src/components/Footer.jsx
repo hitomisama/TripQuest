9c2bb64758f6ffe075d007df83472943697a5e9f
Yisongjing <24aw0105@jec.ac.jp>
2024-12-20 09:30:13 +0900

    完成

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import '../css/footer.css';
 
-
 function Footer() {
-    return(
-<>
-<footer>
-        <h6>運営会社</h6>
+    return (
+        <>
+            <footer>
+            <h6>運営会社</h6>
         <h6>よくある質問</h6>
-        <h6>お問い合わせ</h6>
-</footer>
-
-
-</>
+        <h6>お問い合わせ</h6>            </footer>
+        </>
     );
 }
 
