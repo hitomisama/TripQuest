40216ed97e5f241e0c5b598fa18cddbbdc6d7b50
Yisongjing <24aw0105@jec.ac.jp>
2025-01-16 15:38:05 +0900

    做完了首页轮播图

--- src/components/background.jsx
+++ src/components/background.jsx
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 import '../css/Back.css'; // 引入样式文件
 
 function Back() {
-  const images = ["mv1.png", "mv2.png", "mv3.png", "mv4.png"]; // 图片路径数组
+  const images = ["mv_img.png","mv1.png", "mv2.png", "mv3.png", "mv4.png"]; // 图片路径数组
   const [currentIndex, setCurrentIndex] = useState(0); // 当前显示的图片索引
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length); // 循环切换图片
-    }, 8000); // 每隔 4 秒切换一次图片
+    }, 9000); // 每隔 4 秒切换一次图片
 
     return () => clearInterval(interval); // 组件卸载时清除定时器
   }, [images.length]);
